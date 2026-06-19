2a1cdc828a43ff5557d8e7e5245e0528e563d3e8
Jonathan Aragao Pavam <jonathan.pavam@fernandezpapel.com.br>
2026-06-19 15:43:04 -0300

    feat

--- src/main/java/rest_springboot/rest_spring/exception/UnsuportedMathOperationException.java
+++ src/main/java/rest_springboot/rest_spring/exception/UnsuportedMathOperationException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsuportedMathOperationException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
     public UnsuportedMathOperationException(String message) {
         super(message);
     }
---
+++ src/main/java/rest_springboot/rest_spring/repository/PersonRepository.java
@@ -0,0 +1,6 @@
+package rest_springboot.rest_spring.repository;
+
+import rest_springboot.rest_spring.model.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long>{
+}
